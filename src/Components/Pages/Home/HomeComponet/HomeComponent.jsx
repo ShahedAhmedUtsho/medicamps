84f0c511e1850623f877997815bbedd73bbb9498
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-06-01 02:39:54 +0600

    first commit

---
+++ src/Components/Pages/Home/HomeComponet/HomeComponent.jsx
@@ -0,0 +1,113 @@
+
+import { Download } from "phosphor-react";
+import  { useState } from "react";
+import dotsvg from "../../../../Assets/svg/dotbox.svg"
+import Header from "../../../Header/Header";
+import { Button } from "keep-react";
+
+
+const HomeComponent = () => {
+  return (
+    <div className=" flex flex-col min-h-screen">
+
+      <div className="relative  container  mx-auto pb-[110px] pt-[120px] dark:bg-dark lg:pt-[150px]">
+        <div className="container ">
+          <div className="lg:-mx-4 flex flex-wrap">
+            <div className="w-full px-4 lg:w-5/12">
+              <div className="hero-content ">
+                <h1 className="mb-5 text-4xl font-bold !leading-[1.208] apple text-dark text-slate-900 dark:text-white sm:text-[42px] lg:text-[40px] xl:text-6xl">
+                Transforming Lives Through Medical Camps
+                </h1>
+                
+                <p className="mb-8 max-w-[480px] lg:max-w-[600px] apple text-base lg:text-xl text-body-color dark:text-dark-6">
+                Join us in delivering essential healthcare to communities in need. Our medical camps provide vital services and create inspiring stories of hope and transformation
+                </p>
+                <ul className="flex flex-wrap items-center">
+                  <li>
+                    <a
+                      href="/#"
+                      className="inline-flex items-center justify-center rounded-md bg-primary px-6 py-3 text-center text-base font-medium text-white hover:bg-blue-dark lg:px-7"
+                    >
+                       <Button className="bg-black" size="sm" >Get Started </Button>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/#"
+                      className="inline-flex items-center justify-center px-5 py-3 text-center text-base font-medium text-[#464646] hover:text-primary dark:text-white"
+                    >
+                      <span className="mr-2 ">
+                      <Download size="20" />
+                      </span>
+                      Download App
+                    </a>
+                  </li>
+                </ul>
+                <div className="clients pt-16">
+                  <h6 className="mb-6 flex items-center text-xs font-normal text-body-color dark:text-dark-6">
+                    Some Of Our Clients
+                    <span className="ml-3 inline-block h-px w-8 bg-body-color"></span>
+                  </h6>
+
+                  <div className="flex items-center space-x-4">
+                    <SingleImage
+                      href="#"
+                      imgSrc="https://cdn.tailgrids.com/2.0/image/assets/images/brands/ayroui.svg"
+                    />
+
+                    <SingleImage
+                      href="#"
+                      imgSrc="https://cdn.tailgrids.com/2.0/image/assets/images/brands/graygrids.svg"
+                    />
+
+                    <SingleImage
+                      href="#"
+                      imgSrc="https://cdn.tailgrids.com/2.0/image/assets/images/brands/uideck.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="hidden px-4 lg:block lg:w-1/12"></div>
+            <div className="w-full px-4 lg:w-6/12">
+              <div className="lg:ml-auto lg:text-right">
+                <div className="relative z-10 inline-block pt-11 lg:pt-0">
+
+
+
+                  <img
+                    src="https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png"
+                    alt="hero"
+                    className="max-w-full lg:ml-auto"
+                  />
+
+
+
+                  <span className="absolute  -bottom-8 -left-8 z-[-1]">
+                  <img src={dotsvg} alt="" />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    
+    </div>
+  );
+};
+
+export default HomeComponent;
+
+const SingleImage = ({ href, imgSrc }) => {
+  return (
+    <>
+      <a href={href} className="flex w-full items-center justify-center">
+        <img src={imgSrc} alt="brand image" className="h-10 w-full" />
+      </a>
+    </>
+  );
+};
+
+
---
+++ src/Router/Root.jsx
@@ -0,0 +1,20 @@
+
+import { Outlet } from 'react-router-dom';
+import Header from '../Components/Header/Header';
+
+const Root = () => {
+    return (
+        <div className='min-h-screen flex flex-col relative
+          bg-gradient-to-b from-slate-50 to-blue-400  
+           '>
+             {/* <StyleReset /> */}
+           <div className='mx-auto bg-black relative lg:container '>
+           <Header></Header> 
+           </div>
+            <Outlet />
+            
+        </div>
+    );
+};
+
+export default Root;
