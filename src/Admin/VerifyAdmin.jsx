cda88b002765a62fd44cde12209905505e6e219c
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-06-08 23:17:27 +0600

    fixed veryfy admin

--- src/Admin/VerifyAdmin.jsx
+++ src/Admin/VerifyAdmin.jsx
@@ -4,17 +4,35 @@ import { Navigate, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 import { Spinner } from 'keep-react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 
 const VerifyAdmin = ({ children }) => {
   const location = useLocation();
   const { user, loading } = useContext(AuthContext);
+  const { isLoading,  error, data: MongoUser } = useQuery({
+    queryKey: [`http://localhost:3000/mediusers/${user?.uid}`],
+    queryFn: async () => {
+        const response = await axios.get(`http://localhost:3000/mediusers/${user?.uid}`); 
+        return response.data;
+    }
+});
+
+console.log(MongoUser)
+
+
+
+
+
+// if (isLoading) return <Spinner />;
+if (error) return <div>Error loading data... please try again later.</div>;
 
   if (loading) {
-    return <Spinner className="absolute right-0 left-0 mx-auto top-1/3" color="info" size="xl" />;
+    return <div className=' min-h-screen w-full flex justify-center items-center'><Spinner className="absolute right-0 left-0 mx-auto top-1/3" color="info" size="xl" /></div>;
   }
 
-  if (user && user.isAdmin) {
+  if (user && MongoUser.isAdmin) {
     return children;
   }
 
